dabac424fd5c0fd017343e84399143221c9426dc
Martyna Tołoczko <m.toloczko1@gmail.com>
2021-10-16 14:47:56 +0200

    desktop

--- components/text.tsx
+++ components/text.tsx
@@ -2,7 +2,7 @@ import FadeIn from "react-fade-in";
 
 const Text = () => {
   return (
-    <div className="section c-textSec -pt80">
+    <div className="section c-textSec -pt80sm -pt0">
       <FadeIn delay={200} transitionDuration={600}>
         <p className="-f14">Save the date!</p>
         <hr />
