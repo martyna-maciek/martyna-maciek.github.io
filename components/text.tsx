1401ec317751681cd421efa87163622484a18bd6
Martyna Tołoczko <m.toloczko1@gmail.com>
2021-10-16 14:37:31 +0200

    full design

---
+++ components/text.tsx
@@ -0,0 +1,25 @@
+import FadeIn from "react-fade-in";
+
+const Text = () => {
+  return (
+    <div className="section c-textSec -pt80">
+      <FadeIn delay={200} transitionDuration={600}>
+        <p className="-f14">Save the date!</p>
+        <hr />
+        <p className="-f14 -mb16">
+          Prosimy zarezerwujcie datę <br /> 8 września 2022 roku (czwartek).
+        </p>
+        <p className="-f14 -mb16">
+          Tego dnia powiemy sobie <br /> sakramentalne TAK!
+        </p>
+        <p className="-f14 ">Martyna i Maciek</p>
+        <hr />
+        <p className="-f14">
+          Szczegółowe informacje przekażemy <br /> w późniejszym terminie.
+        </p>
+      </FadeIn>
+    </div>
+  );
+};
+
+export default Text;
