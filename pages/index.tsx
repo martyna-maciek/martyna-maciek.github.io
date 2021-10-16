d20aa8eb37489922e8d57de6eb5d398fb60f8fb6
Martyna Tołoczko <m.toloczko1@gmail.com>
2021-10-16 13:48:47 +0200

    date section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,24 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import styles from "../styles/Home.module.scss";
+import Date from "../components/date";
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>💒 💍 💮</title>
         <meta name="description" content="Save the date kochani!" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Alata&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>Save the date baby!</h1>
-
-        <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+      <main className="main">
+        <Date />
       </main>
     </div>
   );
