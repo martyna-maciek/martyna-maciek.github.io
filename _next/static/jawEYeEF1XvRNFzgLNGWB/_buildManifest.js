cd16a1b67977c14dff40756797a05a7a317c3c0b
warsawgentleman <maciej.matysek1@gmail.com>
2021-10-17 14:48:13 +0200

    Updates

--- _next/static/jawEYeEF1XvRNFzgLNGWB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-f0b5d5252d363467aef9.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
